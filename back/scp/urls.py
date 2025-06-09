053009f8f00753b55cc5d51f048bb11ddcac060d
joao-omati <joao9902@gmail.com>
2025-06-09 08:21:34 -0300

    adicionei o adm na url

--- back/scp/urls.py
+++ back/scp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from usuarios.views import RegisterView, LoginView, LogoutView  # Todas as views estão definidas agora
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
