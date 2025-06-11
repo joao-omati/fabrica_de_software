cb60e807359a79d13c5b5356db25e1633eb56c8f
Guilherme Braga <131495236+guiibrag4@users.noreply.github.com>
2025-06-11 20:06:57 +0000

    Adicionando querys de nível intermediário e nível avançado

--- sql/dml_query.sql
+++ sql/dml_query.sql
@@ -1,3 +1,53 @@
+-- Nível intermediário, agregações e condicionais --
+-- Quantos inscrito vieram de cada tipo de encaminhamento?
+SELECT tipoencaminhamento,
+COUNT(idfichaconvenio) AS total_de_inscritos -- Esta é uma função de agregação. Ela conta o número de linhas para cada grupo. Usar a chave primária (idfichaconvenio) é uma boa prática para garantir a contagem correta.
+FROM inscritoconvenio
+GROUP BY tipoencaminhamento
+ORDER BY total_de_inscritos DESC;
+
+-- Qual supervisor é responsável por quantos estagiários?
+SELECT s.nome AS nome_supervisor, s.crp, COUNT(e.idestagiario) AS quantidade_estagiarios
+FROM supervisor s
+LEFT JOIN estagiario e ON s.crp = e.crpsup
+GROUP BY s.idsupervisor, s.nome, s.crp
+ORDER BY quantidade_estagiarios DESC;
+
+-- Quais religiões tem 2 ou mais inscritos na comunidade?
+SELECT religiao, COUNT(idfichacomunidade) AS total_inscritos
+FROM inscritocomunidade
+GROUP BY religiao
+HAVING COUNT(idfichacomunidade) >= 2 -- Este é o ponto principal. O HAVING é usado para filtrar os grupos que foram criados pelo GROUP BY. Ele é aplicado depois da agregação.
+A condição COUNT(idfichacomunidade) >= 2 diz: "Depois de contar os inscritos em cada grupo de religião, mostre-me apenas os grupos onde essa contagem foi 2 ou maior".
+ORDER BYtotal_inscritos DESC;
+
+-- WHERE vs HAVING
+-- WHERE filtra LINHAS INDIVIDUAIS antes de qualquer agrupamento.
+-- Ex: WHERE status = TRUE (remove linhas individuais que não atendem à condição).
+
+-- HAVING filtra GRUPOS INTEIROS depois do agrupamento e da função de agregação.
+-- Ex: HAVING COUNT(nomecoluna) > 1 (remove grupos inteiros que não atendem à condição).
+
+-- Nível avançado, Subconsultas e múltiplos Joins --
+-- Qual o nome de inscritos da comunidade que têm depressão e também fazem terapia indiivual adulta?
+
+SELECT i.nomeinscrito
+FROM inscritocomunidade AS i
+JOIN motivoacompanhamento AS ma ON i.idfichacomunidade = ma.idfichacomunidade
+JOIN tipoterapia AS tt ON i.idfichacomunidade = tt.idfichacomunidade
+WHERE
+    ma.depressao = TRUE AND tt.individualadt = TRUE;
+
+-- Quais estagiários estão vinculados a inscritos diagnosticados com TEA?
+SELECT DISTINCT -- DISTINCT para não repetir o nome do estagiário (se esse estagiário estiver vinculado a mais de um caso de paciente com TEA, iria aparecer mais vezes sem DISTINCT)
+    e.nome AS nome_estagiario,
+    e.ra
+FROM estagiario AS e
+JOIN escolheins AS ei ON e.idestagiario = ei.idestagiario
+JOIN pcdsnd AS p ON ei.idfichacomunidade = p.idfichacomunidade
+WHERE
+    p.dfa = TRUE;
+
 --Consulta buscando dados da tabela inscritocomunidade com a tabela endereco, onde o nome do inscrito é Lucas Martins Lima
 SELECT i.nomeinscrito, e.cidade, e.rua, e.uf, e.cep
 FROM inscritocomunidade AS i
@@ -41,6 +91,20 @@ WHERE
 	(ma.tristeza = TRUE OR ma.chorofc = TRUE)
 	OR  
 	(pcd.dfa = TRUE OR tt.familia = TRUE OR tt.individualadt = TRUE);
+
+-- Dados dos inscritos da comunidade que moram em Samambaia ou Ceilândia
+SELECT i.nomeinscrito, i.emailinscrito, e.cidade
+FROM inscritocomunidade AS i
+JOIN endereco AS e ON i.idfichacomunidade=e.idfichacomunidade
+WHERE
+    i.idfichacomunidade IN (
+        SELECT idfichacomunidade
+        FROM endereco
+        WHERE cidade IN ('Samambaia', 'Ceilândia')
+    );
+
+
+	
 
 
 	
