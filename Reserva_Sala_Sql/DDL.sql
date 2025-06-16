709292e20ce448747ed19439150d0131516eb527
Aluno <aluno@laboratorio.int>
2025-06-16 18:57:57 -0300

    Foi corrigido as taelas: nti e assessora

--- Reserva_Sala_Sql/DDL.sql
+++ Reserva_Sala_Sql/DDL.sql
@@ -3,16 +3,16 @@ CREATE TABLE assessora (
     idassessora SERIAL PRIMARY KEY,
     nome VARCHAR(50),
     cpf CHAR(11) NOT NULL UNIQUE, 
-    email VARCHAR(100),
-    senha VARCHAR(255),
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
     status BOOLEAN DEFAULT TRUE
 );
 -- Criação de tabela NTI 
 CREATE TABLE nti (
     idnti SERIAL PRIMARY KEY,
     nome VARCHAR(50),
-    cpf CHAR(11) NOT ,
-    email VARCHAR(100),
-    senha VARCHAR(255),
+    cpf CHAR(11) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
     status BOOLEAN DEFAULT TRUE
 );
