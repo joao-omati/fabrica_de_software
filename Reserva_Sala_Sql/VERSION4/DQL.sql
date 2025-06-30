02149a3d99e8c68d2dc94b3235f9248f6aa028b9
vitorguinomio <vitorguinomio@gmail.com>
2025-06-30 22:20:54 +0000

    Fazendo Select com JOIN em turma e curso

--- Reserva_Sala_Sql/VERSION4/DQL.sql
+++ Reserva_Sala_Sql/VERSION4/DQL.sql
@@ -1,4 +1,4 @@
--- Active: 1751319038352@@127.0.0.1@5432@reserva
+-- Active: 1751320626488@@127.0.0.1@5432@reserva
 -- Active: 1751309244752@@127.0.0.1@5432@reserva
 
 
@@ -13,5 +13,18 @@ LIMIT 300;
 SELECT * FROM turma WHERE codturma LIKE('ADS%');
 
 SELECT * FROM turma WHERE codturma LIKE('ADM%');
+
+--Select onde seleciona a turma que esta cursando e todas as turmas desse curso
+
+SELECT
+    t.dthinsert AS data_inserção_turma,
+    t.periodoletivo,
+    t.codturma,
+    c.nomecurso,
+    c.dthinsert AS data_inserção_curso
+FROM 
+    turma t
+JOIN 
+    curso c ON t.idcurso = c.idcurso;
 
 
