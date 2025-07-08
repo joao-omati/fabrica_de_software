7cbd73ed497833faf399f637d17e027efb3a1036
vitorguinomio <vitorguinomio@gmail.com>
2025-07-08 21:10:01 +0000

    foi salva os teste do dia 08/07

--- infra_banco/Reserva_Sala_Sql/VERSION4/DML.sql
+++ infra_banco/Reserva_Sala_Sql/VERSION4/DML.sql
@@ -1,89 +1,25 @@
--- Active: 1751474481388@@127.0.0.1@5432@reserva 
-
--- Active: 1751320626488@@127.0.0.1@5432@reserva
--- Active: 1751309244752@@127.0.0.1@5432@reserva
-
--- USER UM CÓDIGO EM PYTHON PARA INSERIR TODAS AS TURMAS DE UM ARQUIVO.EXEL NESTE CAMINHO: "C:\fabrica_de_software\Reserva_Sala_Sql\VERSION4\importar_dados.py"
-
--- Active: 1751309244752@@127.0.0.1@5432@reserva 
-
-
--- Inserindo usarios 
-
-INSERT INTO usuario (
-    matricula, nome, emailinst, sexo, cargo, senha
-)
+INSERT INTO usario (matricula, matriculauser, nome, emailinst, sexo, cargo, senha) 
 VALUES
-(094147, 'Vitor', 'vitor94147@unieuro.com.br', 'M', 'Diretor', MD5('123456'));
+(94147, NULL, 'Vitor', 'vitor94147@unieuro.com.br', 'M', 'Diretor', 133122),
+(99146, NULL, 'Viceleno', 'viceleno099146@unieuro.com.br', 'F', 'Assessora', 123789),
+(12345, NULL, 'Arthur', 'arthur012345@unieuro.com.br', 'M', 'Coordenador', 987321),
+(21253, NULL, 'ellen', 'ellen021253@unieuro.com.br', 'F', 'Secretaria', 987654),
+(78982, NULL, 'João', 'joao078982@unieuro.com.br', 'M', 'NAPI', 789456),
+(34879, NULL, 'Paulo', 'paulo034879@unieuro.com.br', 'M', 'NTI', 456789),
+(41456, NULL, 'Corno', 'corno041456@unieuro.com.br', 'F', 'Manutenção', 123456);
 
-
---Insert na tabela Sala
 
 INSERT INTO sala (
-    bloco, tvtamanho, datashow, capacidade, andar, numerosala, status
+	bloco, andar, numerosala, capacidade, tvtamanho,datashow
 )
 VALUES
-( 'C', 0, TRUE, 80, '1ª andar', 102, TRUE),
-( 'B', 55, FALSE, 50, '2ª andar', 211, TRUE),
-( 'B', 55, FALSE, 50, '3ª andar', 307, TRUE),
-( 'C', 55, FALSE, 50, '2ª andar', 204, TRUE),
-( 'B', 0, TRUE, 180, '1ª andar', 112, TRUE),
-( 'D', 0, TRUE, 50, 'Térreo', 1, TRUE),
-( 'A', 55, TRUE, 50, 'Térreo', 2, TRUE),
-( 'B', 0, TRUE, 180, 'Térreo', 100, TRUE),
-( 'C', 0, TRUE, 50, '1º Andar', 101, TRUE),
-( 'E', 65, TRUE, 80, '2º Andar', 208, TRUE),
-( 'C', 0, FALSE, 50, '3º Andar', 303, TRUE),
-( 'D', 55, TRUE, 50, '3º Andar', 312, TRUE),
-( 'F', 55, TRUE, 75, '1º Andar', 103, TRUE),
-( 'C', 65, TRUE, 90, '3º Andar', 316, TRUE),
-( 'D', 55, TRUE, 50, '3º Andar', 301, TRUE),
-( 'D', 55, TRUE, 50, '3º Andar', 303, TRUE),
-( 'D', 65, TRUE, 110, '3º Andar', 304, TRUE),
-( 'D', 55, TRUE, 50, '3º Andar', 305, TRUE);
- 
-
---Prenchendo tabela saladispo
-
-INSERT INTO saladispo (
-    idsala, turno    
-)
-VALUES
-(1,'Manhã'),
-(1,'Tarde'),
-(1,'Noite'),
-(2, 'Manhã'),
-(2, 'Tarde'),
-(2, 'Noite'),
-(3, 'Manhã'),
-(3, 'Tarde'),
-(3, 'Noite'),
-(4, 'Manhã'),
-(4, 'Tarde'),
-(4, 'Noite'),
-(5, 'Manhã'),
-(5, 'Tarde'),
-(5, 'Noite'),
-(6, 'Manhã'),
-(6, 'Tarde'),
-(6, 'Noite'),
-(7, 'Manhã'),
-(7, 'Tarde'),
-(7, 'Noite'),
-(8, 'Manhã'),
-(8, 'Tarde'),
-(8, 'Noite'),
-(9, 'Manhã'),
-(9, 'Tarde'),
-(9, 'Noite'),
-(10, 'Manhã'),
-(10, 'Tarde'),
-(10, 'Noite');
+('A', '1º andar', 101, 50, ' 55" ' , FALSE),
+('B', '2º andar', 201, 55, ' 55" ' , FALSE),
+('C', '3º andar', 301, 60, ' 55" ' , FALSE),
+('D', '4º andar', 401, 60, ' 0"  ' , TRUE );
 
 
--- INSERINDO REGISTROS NA ENTIDADE CURSO
-
-INSERT INTO public.curso (nomecurso)
+INSERT INTO curso (nomecurso)
 VALUES
 ('ADMINISTRAÇÃO'),
 ('ANÁLISE E DESENVOLVIMENTO DE SISTEMAS'),
@@ -100,85 +36,26 @@ VALUES
 ('SISTEMAS DE INFORMAÇÃO');
 
 
--- FAZENDO O RELACIONAMENTO ENTRE CURSO E TURMA
+-- simulação de reserva 
 
--- ANALISE DE DESENVOLVIMENTO DE SISTEMAS ADS para os códigos de turmas que começa com ADM
-UPDATE turma SET idcurso = 1 WHERE codturma ILIKE('ADM%');
+--tabela reserva
 
--- ANALISE DE DESENVOLVIMENTO DE SISTEMAS ADS para os códigos de turmas que começa com ADS
-UPDATE turma SET idcurso = 2 WHERE codturma ILIKE('ADS%');
-
-UPDATE turma SET idcurso = 3 WHERE codturma ILIKE('AUR%');
-
-UPDATE turma SET idcurso = 4 WHERE codturma ILIKE('BIO%');
-
-UPDATE turma SET idcurso = 5 WHERE codturma ILIKE('DIR%');
-
-UPDATE turma SET idcurso = 6 WHERE codturma ILIKE('EFB%');
-
-UPDATE turma SET idcurso = 7 WHERE codturma ILIKE('ENF%');
-
-UPDATE turma SET idcurso = 8 WHERE codturma ILIKE('FAR%');
-
-UPDATE turma SET idcurso = 9 WHERE codturma ILIKE('FIS%');
-
-UPDATE turma SET idcurso = 10 WHERE codturma ILIKE('NUT%');
-
-UPDATE turma SET idcurso = 11 WHERE codturma ILIKE('ODO%');
-
-UPDATE turma SET idcurso = 12 WHERE codturma ILIKE('PSI%');
-
-UPDATE turma SET idcurso = 13 WHERE codturma ILIKE('SIN%');
-
-
--- DELETANDO TODOS OS DADOS DA TABELA TURMA PARA RESETAR O ID
-TRUNCATE TABLE turma RESTART IDENTITY CASCADE;
-
-
-
-
-
--- Inserindo nova reserva
-INSERT INTO reserva (
-    matricula, idsaladispo, idcurso, idturma, datainicial, datafinal, responsavel
-)
-VALUES
-(094147, 31, 2, 12 ,'2025-01-12', '2025-06-15', 'Vitor'),
-(094147, 33, 8, 123,'2025-02-10', '2025-07-12', 'Guilherme'),
-(094147, 32, 12, 200,'2025-03-27', '2025-04-09', 'Julio')
+INSERT INTO reserva (matriculauser, idcurso, codturma, datainicial, datafinal)
+VALUES 
+(94147, 1 ,'ADM02N1', '2025-08-11', '2025-11-30')
 RETURNING idreserva;
 
+--tabela reserva sala 
+INSERT INTO reservasala (idreserva, idsala, turno, responsavel)
+VALUES (1, 1, 'Manhã', 'Prof.pamonha') -- turno informado para consistência, mesmo sem períodos
+RETURNING idreservasala;
 
-INSERT INTO diasemana (
-    idreserva, segunda, terca, quarta, quinta, sexta, sabado, domingo
-)
-VALUES
-(1, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE),
-(2, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE),
-(3, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE);
+--tabela periodo
+INSERT INTO periodo (idreservasala, terceiro , quarto)
+VALUES (1, TRUE, TRUE)
+RETURNING idperiodo;
 
+--tabela diasemana
+INSERT INTO diasemana (idreservasala, idperiodo, segunda, terca)
+VALUES (1, 1, TRUE, TRUE);
 
-INSERT INTO periodo (
-    idsaladispo, primeiro, segundo, terceiro, quarto, integral
-)
-VALUES
-(31, TRUE, TRUE, FALSE, FALSE,TRUE),
-(33, FALSE, TRUE, FALSE, FALSE,FALSE),
-(32, FALSE, FALSE, FALSE, FALSE,TRUE);
-
-INSERT INTO tursala(
-    idsala, idturma
-)
-VALUES
-(1,12),
-(1,123),
-(1,200);
-
-
-INSERT INTO reserva_sala(
-    idreserva, idsala, idperiodo, iddiasemana
-)
-VALUES
-(1, 1, 1, 1),
-(2, 1, 2, 2),
-(3, 1, 3, 3);
--- infra_banco/Reserva_Sala_Sql/VERSION4/DQL.sql
+++ infra_banco/Reserva_Sala_Sql/VERSION4/DQL.sql
@@ -1,65 +1,34 @@
--- Active: 1751474481388@@127.0.0.1@5432@reserva 
+SELECT * FROM turma;
 
--- Active: 1751320626488@@127.0.0.1@5432@reserva
--- Active: 1751309244752@@127.0.0.1@5432@reserva
-
-
---Realizando o select das salas
+SELECT * FROM usuario;
 
 SELECT * FROM sala;
 
--- Seleciona tabela saladispo
-SELECT * FROM saladispo;
-
--- Seleciona tabela diasemana
-SELECT * FROM diasemana;
-
---Seleciona tabela reserva
-SELECT * FROM reserva;
-
--- REALIZANDO SELECTS EXPLORATÓRIOS
-
 SELECT * FROM curso;
 
--- Realizando SELECT de 300 registros
+SELECT * FROM reserva;
 
-SELECT * FROM turma
-ORDER BY idturma ASC
-LIMIT 500;
+SELECT * FROM reservasala;
 
+SELECT * FROM periodo;
 
-SELECT * FROM turma WHERE codturma LIKE('ADS%');
+SELECT * FROM diasemana;
 
-SELECT * FROM turma WHERE codturma LIKE('ADM%');
-
-
-
---Select onde seleciona a turma que esta cursando e todas as turmas desse curso
-
-SELECT
-    t.dthinsert AS data_inserção_turma,
-    t.periodoletivo,
-    t.codturma,
-    c.nomecurso,
-    c.dthinsert AS data_inserção_curso
-FROM 
-    turma t
-JOIN 
-    curso c ON t.idcurso = c.idcurso;
+SELECT * FROM periodo;
 
 
 
--- Retorno para saber salas reservadas
+
+-- Select para verificar dados de reserva
 SELECT
   r.datainicial,
   r.datafinal,
-  sd.turno,
-  sd.idsaladispo,
-  s.bloco,
+  r.codturma,
+  rs.turno,
+  rs.responsavel,
+  rs.statusreserva,
   s.numerosala,
-  s.andar,
-  t.codturma,
-  t.periodoletivo,
+  s.bloco,
   p.primeiro,
   p.segundo,
   p.terceiro,
@@ -72,87 +41,90 @@ SELECT
   d.sexta,
   d.sabado,
   d.domingo
-FROM reserva r -- dita que e R um apelido de reserva
-LEFT JOIN saladispo sd ON r.idsaladispo = sd.idsaladispo -- dita que sd representa que SD e apelido de saladispo
-LEFT JOIN sala s ON sd.idsala = s.idsala -- dita que S e um apelido para SERIALIZABLE
-LEFT JOIN turma t ON r.idturma = t.idturma -- dita que T e um apelido de turma
-LEFT JOIN periodo p ON sd.idsaladispo = p.idsaladispo -- dita que P e apelido da tabela periodo
-LEFT JOIN diasemana d ON r.idreserva = d.idreserva -- dita que D e apelido da tabela diasemana
-WHERE r.status = TRUE AND r.situacao = TRUE;
+FROM reserva r 
+LEFT JOIN reservasala rs ON rs.idreserva = r.idreserva
+LEFT JOIN sala s ON rs.idsala = s.idsala
+LEFT JOIN diasemana d ON rs.idreservasala = d.idreservasala
+LEFT JOIN periodo p ON rs.idreservasala = p.idreservasala
+WHERE rs.statusreserva = TRUE;
 
 
--- Seleciona as salas dispo mostra os dias da semana que ela está livre , e em qual periodo
-SELECT * FROM reserva_sala;
+-- Select dos periodos reservados
+SELECT idreservasala, 'segunda' AS periodo FROM periodo WHERE primeiro = TRUE
+UNION
+SELECT idreservasala, 'terca' FROM periodo WHERE segundo = TRUE
+UNION
+SELECT idreservasala, 'quarta' FROM periodo WHERE terceiro = TRUE
+UNION
+SELECT idreservasala, 'quinta' FROM periodo WHERE quarto = TRUE
+UNION
+SELECT idreservasala, 'sexta' FROM periodo WHERE integral = TRUE
+
+-- Select nos dias da semana reservado
+
+SELECT idreservasala, 'segunda' AS dia FROM diasemana WHERE segunda = FALSE
+UNION
+SELECT idreservasala, 'terca' FROM diasemana WHERE terca = FALSE
+UNION
+SELECT idreservasala, 'quarta' FROM diasemana WHERE quarta = FALSE
+UNION
+SELECT idreservasala, 'quinta' FROM diasemana WHERE quinta = FALSE
+UNION
+SELECT idreservasala, 'sexta' FROM diasemana WHERE sexta = FALSE
+UNION
+SELECT idreservasala, 'sabado' FROM diasemana WHERE sabado = FALSE
+UNION
+SELECT idreservasala, 'domingo' FROM diasemana WHERE domingo = FALSE;
+*/
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*WITH combinacoes AS (
-  SELECT
-    s.idsala,
-    s.bloco,
-    s.numerosala,
-    sd.idsaladispo,
-    p.idperiodo,
-    p.primeiro,
-    p.segundo,
-    p.terceiro,
-    p.quarto,
-    p.integral,
-    d.iddiasemana,
-    d.segunda,
-    d.terca,
-    d.quarta,
-    d.quinta,
-    d.sexta,
-    d.sabado,
-    d.domingo
-  FROM sala s
-  JOIN saladispo sd ON s.idsala = sd.idsala
-  JOIN periodo p ON p.idsaladispo = sd.idsaladispo
-  JOIN reserva r ON r.idsaladispo = sd.idsaladispo AND r.status = TRUE AND r.situacao = TRUE
-  JOIN diasemana d ON d.idreserva = r.idreserva
+-- CTEs para extrair dias e períodos marcados
+WITH dias_marcados AS (
+  SELECT idreservasala, 'segunda' AS dia FROM diasemana WHERE segunda = TRUE
+  UNION
+  SELECT idreservasala, 'terca' FROM diasemana WHERE terca = TRUE
+  UNION
+  SELECT idreservasala, 'quarta' FROM diasemana WHERE quarta = TRUE
+  UNION
+  SELECT idreservasala, 'quinta' FROM diasemana WHERE quinta = TRUE
+  UNION
+  SELECT idreservasala, 'sexta' FROM diasemana WHERE sexta = TRUE
+  UNION
+  SELECT idreservasala, 'sabado' FROM diasemana WHERE sabado = TRUE
+  UNION
+  SELECT idreservasala, 'domingo' FROM diasemana WHERE domingo = TRUE
+),
+periodos_marcados AS (
+  SELECT idreservasala, 'primeiro' AS periodo FROM periodo WHERE primeiro = TRUE
+  UNION
+  SELECT idreservasala, 'segundo' FROM periodo WHERE segundo = TRUE
+  UNION
+  SELECT idreservasala, 'terceiro' FROM periodo WHERE terceiro = TRUE
+  UNION
+  SELECT idreservasala, 'quarto' FROM periodo WHERE quarto = TRUE
+  UNION
+  SELECT idreservasala, 'integral' FROM periodo WHERE integral = TRUE
 )
+
+-- Consulta principal unindo tudo
 SELECT
-  c.idsala,
-  c.bloco,
-  c.numerosala,
-  c.idperiodo,
-  unnest(ARRAY[
-    CASE WHEN c.segunda = FALSE THEN 'Segunda' ELSE NULL END,
-    CASE WHEN c.terca = FALSE THEN 'Terça' ELSE NULL END,
-    CASE WHEN c.quarta = FALSE THEN 'Quarta' ELSE NULL END,
-    CASE WHEN c.quinta = FALSE THEN 'Quinta' ELSE NULL END,
-    CASE WHEN c.sexta = FALSE THEN 'Sexta' ELSE NULL END,
-    CASE WHEN c.sabado = FALSE THEN 'Sábado' ELSE NULL END,
-    CASE WHEN c.domingo = FALSE THEN 'Domingo' ELSE NULL END
-  ]) AS dia_livre
-FROM combinacoes c
-WHERE (
-    NOT c.segunda OR NOT c.terca OR NOT c.quarta OR
-    NOT c.quinta OR NOT c.sexta OR NOT c.sabado OR
-    NOT c.domingo
-)
-ORDER BY c.idsala, c.idperiodo, dia_livre;*/
+  r.datainicial,
+  r.datafinal,
+  r.codturma,
+  rs.turno,
+  rs.responsavel,
+  rs.statusreserva,
+  s.numerosala,
+  s.bloco,
+  d.dia,
+  p.periodo
+FROM reserva r 
+LEFT JOIN reservasala rs ON rs.idreserva = r.idreserva
+LEFT JOIN sala s ON rs.idsala = s.idsala
+LEFT JOIN dias_marcados d ON rs.idreservasala = d.idreservasala
+LEFT JOIN periodos_marcados p ON rs.idreservasala = p.idreservasala
+WHERE rs.statusreserva = TRUE;
+
+
